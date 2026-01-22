fa7740d7f2faff8018ef2e9866401a5061a59cb5
ahmed mrsawy <mrsawy1@gmail.com>
2026-01-22 07:24:50 +0200

    ui apdates with magic ui

--- apps/client/src/components/molecules/theme-switcher.tsx
+++ apps/client/src/components/molecules/theme-switcher.tsx
@@ -11,31 +11,33 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/atoms/dropdown-menu"
+import { AnimatedThemeToggler } from "../atoms/animated-theme-toggler"
 
 export function ModeToggle({ className }: { className?: string }) {
     const { setTheme ,theme } = useTheme()
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild className={className}>
-                <Button variant="outline" size="icon" >
-                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                    <span className="sr-only" > Toggle theme </span>
-                </Button>
-            </DropdownMenuTrigger>
-            < DropdownMenuContent align="end" >
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                </DropdownMenuItem>
-                < DropdownMenuItem onClick={() => setTheme("dark")
-                }>
-                    Dark
-                </DropdownMenuItem>
-                < DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <AnimatedThemeToggler />
     )
+        // <DropdownMenu>
+        //     <DropdownMenuTrigger asChild className={className}>
+        //         <Button variant="outline" size="icon" >
+        //             <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        //             <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        //             <span className="sr-only" > Toggle theme </span>
+        //         </Button>
+        //     </DropdownMenuTrigger>
+        //     < DropdownMenuContent align="end" >
+        //         <DropdownMenuItem onClick={() => setTheme("light")}>
+        //             Light
+        //         </DropdownMenuItem>
+        //         < DropdownMenuItem onClick={() => setTheme("dark")
+        //         }>
+        //             Dark
+        //         </DropdownMenuItem>
+        //         < DropdownMenuItem onClick={() => setTheme("system")}>
+        //             System
+        //         </DropdownMenuItem>
+        //     </DropdownMenuContent>
+        // </DropdownMenu>
 }
--- apps/client/src/components/organs/home-nav.tsx
+++ apps/client/src/components/organs/home-nav.tsx
@@ -2,19 +2,24 @@
 import { IUser } from '@/lib/types/user/user.interface';
 import { Navbar02 } from '../molecules/nav-bar';
 import { useRouter } from '@/i18n/navigation';
+import { ScrollProgress } from '../atoms/scroll-progress';
 
 function HomeNave({ user }: { user?: IUser }) {
   const router = useRouter();
   return (
-    <Navbar02
-      user={user}
-      onSignInClick={() => {
-        router.push('/login');
-      }}
-      onCtaClick={() => {
-        router.push('/signup');
-      }}
-    />
+    <>
+      <Navbar02
+        user={user}
+        onSignInClick={() => {
+          router.push('/login');
+        }}
+        onCtaClick={() => {
+          router.push('/signup');
+        }}
+      />
+      <ScrollProgress className='top-16' />
+    </>
+
   );
 }
 
